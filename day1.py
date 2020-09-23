53fae2f67304548bf813e8faa164ba4838144cf9
Yusuf <yusuf.salie96@mail.com>
2020-09-23 14:46:40 +0200

    commit

--- day1.py
+++ day1.py
@@ -1,9 +1,18 @@
+#prints hello world
 print("hello world")
 
-class_name = input("class number name\n")
+#ask class name
+class_name = input("class number:\n")
 
-print("I was coded in " + class_name)
+#asks number of students
+class_students = input("number of students:\n")
 
-class_students = input("number of students\n")
+#asks name
+student_name = input("what is your name:\n")
 
-print("there are " + class_students + " students in " + class_name)
+#ask age
+student_age = input("what is your age?")
+
+print("so you are " + student_name + "and your age is" + student_age + " there are " + class_students + " students in " + class_name)
+
+
